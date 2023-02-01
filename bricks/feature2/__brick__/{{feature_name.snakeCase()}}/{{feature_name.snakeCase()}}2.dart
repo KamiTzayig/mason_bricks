a706f473495f6cd997ae3786435abc9ca8a57df4
KamiTzayig <Dyekozz@gmail.com>
2023-02-01 21:05:27 +0200

    feature fix maybe 19

--- bricks/feature2/__brick__/{{feature_name.snakeCase()}}/{{feature_name.snakeCase()}}2.dart
+++
@@ -1,3 +0,0 @@
-{{#isBloc}}export 'bloc/bloc.dart';{{/isBloc}}{{#isCubit}}export 'cubit/cubit.dart';{{/isCubit}}{{#isProvider}}export 'provider/provider.dart';{{/isProvider}}{{#isRiverpod}}export 'provider/provider.dart';{{/isRiverpod}}
-export 'view/{{feature_name.snakeCase()}}_page.dart';
-export 'widgets/widgets.dart';
--- bricks/feature_tests/__brick__/{{feature_name.snakeCase()}}/view/{{feature_name.snakeCase()}}_page_test.dart
+++ bricks/feature_tests/__brick__/{{feature_name.snakeCase()}}/view/{{feature_name.snakeCase()}}_page_test.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:{{{fullPath}}}/{{feature_name.snakeCase()}}2.dart';
+import 'package:{{{fullPath}}}/{{feature_name.snakeCase()}}.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
