e6b9947a2713e5f2fff9bf97b41dc86d7aadfa46
KamiTzayig <Dyekozz@gmail.com>
2023-02-01 19:41:51 +0200

    feature fix maybe 5

--- bricks/feature2/__brick__/test/{{feature_name.snakeCase()}}/domain/{{feature_name.snakeCase()}}_repository.dart
+++
@@ -1,4 +0,0 @@
-//example of an abstract repository which the data layer will implement
-abstract class {{feature_name.pascalCase()}}Repository {
-  Future<bool> ping({required String url});
-}
--- bricks/feature2/__brick__/test/{{feature_name.snakeCase()}}/{{feature_name.snakeCase()}}.dart
+++
@@ -1,3 +0,0 @@
-{{#isBloc}}export 'application/bloc/bloc.dart';{{/isBloc}}{{#isCubit}}export 'application/cubit/cubit.dart';{{/isCubit}}{{#isProvider}}export 'application/state.dart';{{/isProvider}}{{#isRiverpod}}export 'application/provider/state.dart';{{/isRiverpod}}
-export 'view/{{feature_name.snakeCase()}}_page.dart';
-export 'widgets/widgets.dart';
