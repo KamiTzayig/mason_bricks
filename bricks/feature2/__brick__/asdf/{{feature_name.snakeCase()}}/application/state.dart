7726218b2dc90c58c838faaafb36eb0a021eef57
KamiTzayig <Dyekozz@gmail.com>
2023-02-01 20:43:08 +0200

    feature fix maybe 11

---
+++ bricks/feature2/__brick__/asdf/{{feature_name.snakeCase()}}/application/state.dart
@@ -0,0 +1,4 @@
+export 'package:hooks_riverpod/hooks_riverpod.dart';
+export 'notifiers/{{feature_name.snakeCase()}}_state_notifier.dart';
+export 'providers/{{feature_name.snakeCase()}}_state_provider.dart';
+
---
+++ bricks/feature2/__brick__/asdf/{{feature_name.snakeCase()}}/data/{{feature_name.snakeCase()}}_repository_implementation.dart
@@ -0,0 +1,15 @@
+import '../domain/{{feature_name.snakeCase()}}_repository.dart';
+
+
+class {{feature_name.pascalCase()}}RepositoryImplementation implements {{feature_name.pascalCase()}}Repository{
+  //example of implementation of a function
+  //just the concept. the code doesnt work
+  @override
+  Future<bool> ping({required String url}) async{
+    var response = await http.get(Uri.parse(url))
+    return response.statusCode == "200"? True: False;
+
+}
+
+
+}
